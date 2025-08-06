579ea7cb62b1ae5381dd3308d83b1190f5c3958c
Mohit Thapa <thapamohit2058@gmail.com>
2025-08-06 23:21:31 +0545

    frontend initialized

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from "react";
+
+interface ButtonProps {
+    variant:"primary" | "secondary";
+    text:string;
+    startIcon:ReactElement
+}
+
+export function Button(props:ButtonProps) {
+
+}
