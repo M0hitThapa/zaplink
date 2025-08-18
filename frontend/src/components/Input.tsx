182efbeb6af1aee30ce5b0d3cc8fa66edea059e6
Mohit Thapa <thapamohit2058@gmail.com>
2025-08-18 17:24:41 +0545

    signup route added with backend

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,6 +1,6 @@
 
-export function Input({onChange,placeholder}: {placeholder:string;onChange:() => void}) {
+export function Input({reference,placeholder}: {placeholder:string;reference:any}) {
     return (
-        <input placeholder={placeholder} type="text" className="px-8 py-3 border-2 border-slate-300 shadow-md" />
+        <input ref={reference} placeholder={placeholder} type="text" className="px-8 py-3 border-2 border-slate-300 shadow-md" />
     )
 }
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,12 +1,27 @@
+import { useRef } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export function SignUp() {
+const usernameRef = useRef<HTMLInputElement>(null)
+const passwordRef = useRef<HTMLInputElement>(null)
+    function signup () {
+const username = usernameRef.current?.value;
+const password = passwordRef.current?.value;
+axios.post(BACKEND_URL + "/api/v1/signup", {
+    username,
+    password
+})
+alert("signup completed")
+
+    }
     return <div className="h-screen w-screen flex justify-center items-center bg-gray-200 ">
         <div className="bg-white shadow-md p-5 rounded-xl border border-gray-100 flex flex-col gap-2 ">
-            <Input placeholder="Username" />
-            <Input placeholder="Password" />
-            <Button text="SignUp" variant="primary" />
+            <Input reference={usernameRef} placeholder="Username" />
+            <Input reference={passwordRef} placeholder="Password" />
+            <Button onClick={signup} text="SignUp" variant="primary" />
 
         </div>
 
