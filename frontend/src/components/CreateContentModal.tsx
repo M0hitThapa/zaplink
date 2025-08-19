3f6e04f0f2a7e61222bcde260f892575a32a27ec
Mohit Thapa <thapamohit2058@gmail.com>
2025-08-19 20:24:59 +0545

    connected createcontentmodal to backend and able to upload url through frontend

--- frontend/src/components/CreateContentModal.tsx
+++ frontend/src/components/CreateContentModal.tsx
@@ -1,8 +1,33 @@
+import { useRef, useState } from "react"
 import { CloseIcon } from "../icons/closeIcon"
 import { Button } from "./Button"
 import { Input } from "./Input"
+import { BACKEND_URL } from "../config"
+import axios from "axios"
+
+enum ContentModal {
+    Youtube = "youtube",
+    Twitter = "twitter"
+}
 
 export const CreateContextModal = ({open, onClose}) => {
+    const titleRef = useRef<HTMLInputElement>(null)
+    const linkRef = useRef<HTMLInputElement>(null)
+    const [type, setType] = useState(ContentModal.Youtube)
+
+    async function addContent () {
+        const title = titleRef.current?.value;
+        const link = linkRef.current?.value;
+        await axios.post(BACKEND_URL + "/api/v1/content", {
+            link,
+            title,
+            type
+        }, {
+            headers: {
+                "Authorization":localStorage.getItem("token")
+            }
+        })
+    }
     return (
         <div>
             {open && <div className="h-screen w-screen fixed bg-gray-500/60 flex items-center justify-center z-[100]">
@@ -13,11 +38,19 @@ export const CreateContextModal = ({open, onClose}) => {
                     </div>
                 </div>
                 <div className="flex flex-col gap-2 mt-3">
-                    <Input placeholder="title" />
-                    <Input placeholder="link" />
+                    <Input reference={titleRef} placeholder="title" />
+                    <Input reference={linkRef} placeholder="link" />
+                    <div className="flex gap-2 items-center justify-center mt-5">
+                        <Button text="youtube" variant={type === ContentModal.Youtube ? "primary" : "secondary"} onClick={() => {
+                            setType(ContentModal.Youtube)
+                        }} />
+                        <Button text="twitter" variant={type === ContentModal.Twitter ? "primary" :"secondary"} onClick={() => {
+                            setType(ContentModal.Twitter)
+                        }} />
+                    </div>
                 </div>
                 <div className="flex justify-center mt-2">
-                    <Button variant="primary" text="submit" />
+                    <Button onClick={addContent} variant="primary" text="submit" />
                 </div>
                 
 
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -3,10 +3,13 @@ import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export function SignIn() {
     const usernameRef = useRef<HTMLInputElement>(null)
     const passwordRef = useRef<HTMLInputElement>(null)
+
+    const navigate = useNavigate()
 async function signin() {
     const username = usernameRef.current?.value;
     const password = passwordRef.current?.value;
@@ -17,6 +20,8 @@ async function signin() {
     })
     const jwt = response.data.token;
     localStorage.setItem("token", jwt)
+    navigate("/dashboard")
+    
 }
     return <div className="h-screen w-screen flex justify-center items-center bg-gray-200 ">
         <div className="bg-white shadow-md p-5 rounded-xl border border-gray-100 flex flex-col gap-2 ">
